f2eccad43c82f4456cdf45264cf6774093f1b540
thibaultrousset <thibaultrousset94@gmail.com>
2023-09-18 14:15:44 +0200

    init: librarys & facebook auth & firebase

---
+++ lib/state/auth/constants/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class Constants {
+  static const accountExistsWithDifferentCredential =
+      'account-exists-with-different-credential';
+  static const emailScope = 'email';
+  const Constants._();
+}
---
+++ lib/state/auth/models/auth_result.dart
@@ -0,0 +1,5 @@
+enum AuthResult {
+  abordted,
+  success,
+  failure,
+}
